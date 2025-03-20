65b6ee34d63f47488f3c2325f8859e1dd11ff0f6
ssj <facuperbak72@gmail.com>
2025-03-20 20:27:37 -0300

    ;

--- clases.cpp
+++ clases.cpp
@@ -5,82 +5,83 @@ DTFecha::DTFecha(){
 	this->dia = 0;
 	this->mes = 0;
 	this->anio = 0;
-};
+}
 
 DTFecha::DTFecha(int dia, int mes, int anio){
 	this->dia = dia;
 	this->mes = mes;
 	this->anio = anio;
-};
+}
 
-DTFecha::~DTFecha(){};
+DTFecha::~DTFecha(){}
 
 int DTFecha::getDia(){
 	return dia;
-};
+}
 
 int DTFecha::getMes(){
 	return mes;
-};
+}
+
 int DTFecha::getAnio(){
 	return anio;
-};
+}
 
 void DTFecha::setDia(int dia){
 	this->dia = dia;
-};
+}
 
 void DTFecha::setMes(int mes){
 	this->mes = mes;
-};
+}
 
 void DTFecha::setAnio(int anio){
 	this->anio = anio;
 }
 
 //---------------------- Operaciones DTRefer ---------------------//
-DTRefer::DTRefer(){};
+DTRefer::DTRefer(){}
 
 DTRefer::DTRefer(string DOI, string titulo, DTFecha fecha, set<string> autores){
 	this->DOI = DOI;
 	this->titulo = titulo;
 	this->fecha = fecha;
 	this->autores = autores;
-};
+}
 
 DTRefer::~DTRefer(){}
 
 string DTRefer::getDOI(){
 	return this->DOI;
-};
+}
 
 string DTRefer::getTitulo(){
 	return this->titulo;
-};
+}
 
 DTFecha DTRefer::getFecha(){
 	return this->fecha;
-};
+}
 
 set<string> DTRefer::getAutores(){
     return this->autores;
-};
+}
 
 void DTRefer::setDOI(string DOI){
     this->DOI = DOI;
-};
+}
 
 void DTRefer::setTitulo(string titulo){
     this->titulo = titulo;
-};
+}
 
 void DTRefer::setFecha(DTFecha fecha){
     this->fecha = fecha;
-};
+}
 
 void DTRefer::setAutores(const set<string>& autores){
     this->autores = autores;
-};
+}
 
 ostream& operator << (ostream& cout, DTRefer& ref){
 	cout<<ref.getDOI()<<"->"<<ref.getTitulo()<<"("
@@ -98,47 +99,47 @@ ostream& operator << (ostream& cout, DTRefer& ref){
 };
 
 //-------------------Operaciones Publicacion-----------------------//
-Publicacion::Publicacion(){};
+Publicacion::Publicacion(){}
 
 Publicacion::Publicacion(string DOI, string titulo, DTFecha fecha){
 	this->DOI = DOI;
 	this->titulo = titulo;
 	this->fecha = fecha;
-};
+}
 
-Publicacion::~Publicacion(){};
+Publicacion::~Publicacion(){}
 
 string Publicacion::getDOI(){ 
 	return DOI;
-};
+}
 
 string Publicacion::getTitulo(){ 
 	return titulo; 
-};
+}
 
 DTFecha Publicacion::getFecha(){ 
 	return fecha;
-};
+}
 
 set<Investigador*> Publicacion::getSuInvestigador(){
 	return suInvestigador;
-};
+}
 
 void Publicacion::setDOI(string DOI){
 	this->DOI = DOI;
-};
+}
 
 void Publicacion::setTitulo(string titulo){
 	this->titulo = titulo;
-};
+}
 
 void Publicacion::setFecha(DTFecha fecha){
 	this->fecha = fecha;
-};
+}
 
 void Publicacion::setSuInvestigador(set<Investigador*> suInvestigador){
 	this->suInvestigador = suInvestigador;
-};
+}
 
 DTRefer Publicacion::getDT() {
     set<string> nombresAutores;
@@ -146,11 +147,11 @@ DTRefer Publicacion::getDT() {
         nombresAutores.insert(autor->toString());
     }
     return DTRefer(DOI, titulo, fecha, nombresAutores);
-};
+}
 
 void Publicacion::agregarInvestigador(Investigador* investigador){
     inv.insert(investigador);
-};
+}
 
 bool Publicacion::contienePalabra(string palabra){}; // Operacion abstracta
 
@@ -167,19 +168,19 @@ ArticuloRevista::~ArticuloRevista(){};
 
 void ArticuloRevista::setRevista(string revista){
 	this->revista = revista;
-};
+}
 
 void ArticuloRevista::setExtracto(string extracto){
 	this->extracto = extracto;
-};
+}
 
 string ArticuloRevista::getRevista(){
 	return revista;	
-};
+}
 
 string ArticuloRevista::getExtracto(){
 	return extracto;	
-};
+}
 
 bool ArticuloRevista::contienePalabra(string palabra) {
     return extracto.find(palabra) != std::string::npos;
@@ -192,29 +193,29 @@ Libro::Libro(){};
 Libro::Libro(string editorial, set<string> palDest){
 	this->editorial = editorial;
 	this->palabrasDestacadas = palDest;
-};
+}
 
 Libro::~Libro(){};
 
 void Libro::setEditorial(string editorial){
 	this->editorial = editorial;
-};
+}
 
 void Libro::setPalDest(set<string> palDest){
 	this->palabrasDestacadas = palDest;
-};
+}
 
 string Libro::getEditorial(){
 	return editorial;	
-};
+}
 
 set<string> Libro::getPalDest(){
 	return palabrasDestacadas;	
-};
+}
 
 bool Libro::contienePalabra(string palabra){
 	return palabrasDestacadas.find(palabra) != palabrasDestacadas.end();
-};
+}
 
 
 //---------------------------------- Operaciones PaginaWeb ----------------------------------//
@@ -223,29 +224,29 @@ PaginaWeb::PaginaWeb(){};
 PaginaWeb::PaginaWeb(string url,string contExt){
 	this->url = url;
 	this->contenidoExtraido = contExt;
-};
+}
 
-PaginaWeb::~PaginaWeb(){};
+PaginaWeb::~PaginaWeb(){}
 
 bool PaginaWeb::contienePalabra(string palabra){
 	return contenidoExtraido.find(palabra) != std::string::npos;
-};
+}
 
 string PaginaWeb::getUrl(){
 	return url;
-};
+}
 
 string PaginaWeb::getContenidoExtraido(){
 	return contenidoExtraido;
-};
+}
 
 void PaginaWeb::setUrl(string url){
 	this->url = url;
-};
+}
 
 void PaginaWeb::setContenidoExtraido(string contenidoExtraido){
 	this->contenidoExtraido = contenidoExtraido;
-};
+}
 
 // ------------------------------ Operaciones Investigador -----------------------------------//
 Investigador::Investigador(){};
@@ -254,21 +255,21 @@ Investigador::Investigador(string ORCID, string nombre, string institucion){
 	this->ORCID = ORCID;
 	this->nombre = nombre;
 	this->institucion = institucion;
-};
+}
 
 Investigador::~Investigador(){};
 
 string Investigador::getORCID(){
 	return ORCID;
-};
+}
 
 string Investigador::getNombre(){
 	return nombre;
-};
+}
 
 string Investigador::getInstitucion(){
 	return institucion;
-};
+}
 
 set<Publicacion*> Investigador::getSusPublicaciones(){
 	return susPublicaciones;
@@ -276,23 +277,23 @@ set<Publicacion*> Investigador::getSusPublicaciones(){
 
 void Investigador::setORCID(string ORCID){
 	this->ORCID = ORCID;
-};
+}
 
 void Investigador::setNombre(string nombre){
 	this->nombre = nombre;
-};
+}
 
 void Investigador::setInstitucion(string institucion){
 	this->institucion = institucion;
-};
+}
 
 void Investigador::setSusPublicaciones(string ORCID, string nombre, string institucion){
 	this->susPublicaciones = susPublicaciones;
-};
+}
 
 string Investigador::toString(){
 	return ORCID + "->" + nombre + "/" + institucion;
-};
+}
 
 // class Publicacion{
 // 	protected:
@@ -310,7 +311,7 @@ string Investigador::toString(){
 // 		DTRefer getDT();
 // 		void agregarInvestigador(Investigador* investigador);
 // 		virtual bool contienePalabra(string palabra) = 0 ;
-// };
+// }
 
 set<string> Investigador ::listarPublicaciones(DTFecha desde,string palabra){
 	// set<string> nuevoSet;
@@ -320,4 +321,4 @@ set<string> Investigador ::listarPublicaciones(DTFecha desde,string palabra){
 	// 	}
 	// }
     // return DTRefer(DOI, titulo, fecha, nombresAutores);
-};
+}
--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@
 
 using namespace std;
 
-std::list<Publicacion*> publicaciones;
-std::map<std::string, Publicacion*> map_publicaciones;
+list<Publicacion*> publicaciones;
+map<string, Publicacion*> map_publicaciones;
 
-std::list<Investigador*> investigadores;
-std::map<std::string, Investigador*> map_investigadores;
+list<Investigador*> investigadores;
+map<string, Investigador*> map_investigadores;
 
 void coleccion_guardarPublicacion(Publicacion* pub){
 	publicaciones.push_back(pub);
-	std::pair<std::string, Publicacion*> entry(pub->getDOI(), pub);
+	pair<string, Publicacion*> entry(pub->getDOI(), pub);
     map_publicaciones.insert(entry);
 }
 void coleccion_eliminarPublicacion(Publicacion* pub){
@@ -25,19 +25,32 @@ void coleccion_eliminarPublicacion(Publicacion* pub){
 
 void coleccion_guardarInvestigador(Investigador* inv){
 	investigadores.push_back(inv);
-	std::pair<std::string, Investigador*> entry(inv->getORCID(), inv);
+	pair<string, Investigador*> entry(inv->getORCID(), inv);
     map_investigadores.insert(entry);
 }
 
-Investigador* coleccion_getInvestigador(std::string ORCID){
+Investigador* coleccion_getInvestigador(string ORCID){
 	return map_investigadores[ORCID];
 }
 
-Publicacion* coleccion_getPublicacion(std::string DOI){
+Publicacion* coleccion_getPublicacion(string DOI){
 	return map_publicaciones[DOI];
 }
 
 void parte_a(){
+	// DTFecha *fecha1 = new DTFecha(15,5,2023);
+	// ArticuloRevista *art1 = new ArticuloRevista("Programacion Avanzada", "Introduccion a los principios fundamentales de la programacion orientada a objetos, explicando sus conceptos clave como clases, objetos, herencia y polimorfismo.");
+
+	// art1->setDOI("10.1234/abc123");
+	// art1->setTitulo("Fundamentos de POO");
+	// art1->setFecha(*fecha1);
+	
+	// DTFecha *fecha2 = new DTFecha(10,2,2024);
+	// ArticuloRevista *art2 = new ArticuloRevista("Modelado de Software", "Ejercicio empirico de como los diagramas UML pueden ayudar en el proceso y documentacion de software, cubriendo los tipos mas importantes utilizados, como clases.");
+
+	// art2->setDOI("10.4567/jkl012");
+	// art2->setTitulo("Utilidad de diagramas UML");
+	// art2->setFecha(*fecha2);
 }
 
 void parte_b(){
@@ -72,31 +85,31 @@ void parte_k(){
 
 
 int main() {
-	std::cout << "parte_a" <<  std::endl;
+	cout << "parte_a" <<  endl;
 	parte_a();
-	std::cout << "parte_b" <<  std::endl;
+	cout << "parte_b" <<  endl;
 	parte_b();
-	std::cout << "parte_c" <<  std::endl;
+	cout << "parte_c" <<  endl;
 	parte_c();
-	std::cout << "parte_d" <<  std::endl;
+	cout << "parte_d" <<  endl;
 	parte_d();
-	std::cout << "parte_e" <<  std::endl;
+	cout << "parte_e" <<  endl;
 	parte_e();
-	std::cout << "parte_f" <<  std::endl;
+	cout << "parte_f" <<  endl;
 	parte_f();
-	std::cout << "parte_g" <<  std::endl;
+	cout << "parte_g" <<  endl;
 	parte_g();
-	std::cout << "parte_h" <<  std::endl;
+	cout << "parte_h" <<  endl;
 	parte_h();
-	std::cout << "parte_i" <<  std::endl;
+	cout << "parte_i" <<  endl;
 	parte_i();
-	std::cout << "parte_j" <<  std::endl;
+	cout << "parte_j" <<  endl;
 	parte_j();
-	std::cout << "parte_k" <<  std::endl;
+	cout << "parte_k" <<  endl;
 	parte_k();
-	std::cout << "cleanUp" <<  std::endl;
+	cout << "cleanUp" <<  endl;
 	cleanUp();
-	std::cout << "fin" <<  std::endl;
+	cout << "fin" <<  endl;
 
 	return 0;
 }
